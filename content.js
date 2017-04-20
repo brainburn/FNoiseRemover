b0130b0828c940e9a1c06017b92e96dcb31492f6
Dude <nikola.chonkov@gmail.com>
2017-04-19 21:29:39 -0400

    asd

--- content.js
+++ content.js
@@ -1,5 +1,1 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-//alert("Hello from your Chrome extension!")
 document.getElementById("content_container").style.visibility='hidden'
